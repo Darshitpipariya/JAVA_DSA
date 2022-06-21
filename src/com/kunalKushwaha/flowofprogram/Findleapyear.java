fd173c0ca715bed988a06f672ccbb4dbc750277b
DARSHIT PIPARIYA <38787348+Darshitpipariya@users.noreply.github.com>
2022-06-21 18:10:03 +0530

    new file

--- src/com/kunalKushwaha/flowofprogram/Findleapyear.java
+++ src/com/kunalKushwaha/flowofprogram/Findleapyear.java
@@ -1,2 +1,15 @@
-package com.kunalKushwaha.flowofprogram;public class Findleapyear {
+package com.kunalKushwaha.flowofprogram;
+
+import java.util.Scanner;
+
+public class Findleapyear {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int input=sc.nextInt();
+        if((input%4==0 && input%100!=0) ||input%400==0){
+            System.out.println("given year is leap year");
+        } else {
+            System.out.println("not Leap year");
+        }
+    }
 }
